c5833b738d236519d9151917375512068ddde279
piotrkowalczykk <kowalczyk.piotrt@gmail.com>
2024-11-20 20:21:01 +0100

    DataIntegrityViolationException handled

--- src/main/java/com/user_auth/user_auth/controller/GlobalExceptionController.java
+++ src/main/java/com/user_auth/user_auth/controller/GlobalExceptionController.java
@@ -1,5 +1,6 @@
 package com.user_auth.user_auth.controller;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,10 +13,18 @@ import java.util.stream.Collectors;
 public class GlobalExceptionController {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException exception) {
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
         String errorMessage = exception.getBindingResult().getFieldErrors().stream()
                 .map(error -> error.getField() + ": " + error.getDefaultMessage())
                 .collect(Collectors.joining(", "));
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException exception){
+        String errorMessage = exception.getMessage();
+        if(errorMessage.contains("Duplicate entry"))
+            return new ResponseEntity<>("email: Email already exists", HttpStatus.BAD_REQUEST);
         return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 
