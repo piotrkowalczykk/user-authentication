43a5830e7e390d6b83b1b17e4569fa4f9df07464
piotrkowalczykk <kowalczyk.piotrt@gmail.com>
2024-11-20 22:21:02 +0100

    jwt

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,10 +20,15 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("mysql:mysql-connector-java:8.0.33")
 	implementation("org.springframework.boot:spring-boot-starter-validation:3.3.5")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+	implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
+	implementation(("io.jsonwebtoken:jjwt-jackson:0.12.6"))
+
+	implementation("mysql:mysql-connector-java:8.0.33")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
